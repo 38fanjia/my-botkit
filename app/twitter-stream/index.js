28c13e0fd94a271dfba98651a18263901d8cf928
38fanjia <siida623+github@gmail.com>
2017-02-25 00:07:32 +0900

    :+1: twitter stream から slack へ投稿する際の、投稿先のチャンネルと filter の設定を環境変数で設定するように変更する

--- app/twitter-stream/index.js
+++ app/twitter-stream/index.js
@@ -9,8 +9,8 @@ const twitterclient = Twitter({
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-if (!process.env.CONSUMER_KEY || !process.env.CONSUMER_SECRET || !process.env.ACCESS_TOKEN_KEY, !process.env.ACCESS_TOKEN_SECRET) {
-  console.log('Error: Specify CONSUMER_KEY CONSUMER_SECRET ACCESS_TOKEN_KEY ACCESS_TOKEN_SECRET in environment');
+if (!process.env.CONSUMER_KEY || !process.env.CONSUMER_SECRET || !process.env.ACCESS_TOKEN_KEY || !process.env.ACCESS_TOKEN_SECRET || !process.env.STREAM_FILTER || !process.env.STREAM_CHANNEL) {
+  console.log('Error: Specify CONSUMER_KEY CONSUMER_SECRET ACCESS_TOKEN_KEY ACCESS_TOKEN_SECRET STREAM_FILTER STREAM_CHANNEL in environment');
   process.exit(1);
 }
 
@@ -21,14 +21,14 @@ export default class TwitterStream {
   }
 
   stream() {
-    twitterclient.stream('statuses/filter', {track: '<keyword>'}, (stream) => {
+    twitterclient.stream('statuses/filter', {track: process.env.STREAM_FILTER}, (stream) => {
 
       stream.on('data', (tweet) => {
         const url = 'http://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str;
         this.bot.api.chat.postMessage({
           text: url,
-          channel: '<channel id>',
-          username: 'bot',
+          channel: process.env.STREAM_CHANNEL,
+          username: 'twitter',
           icon_emoji: ':bird:'
         });
       });
