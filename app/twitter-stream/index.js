f0beee03de390653119e39aabf9f4ab030220832
38fanjia <siida623+github@gmail.com>
2017-02-27 23:27:19 +0900

    :+1: twitter stream の投稿先を、 channel name を指定するように変更する

--- app/twitter-stream/index.js
+++ app/twitter-stream/index.js
@@ -9,8 +9,8 @@ const twitterclient = Twitter({
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-if (!process.env.CONSUMER_KEY || !process.env.CONSUMER_SECRET || !process.env.ACCESS_TOKEN_KEY || !process.env.ACCESS_TOKEN_SECRET || !process.env.STREAM_FILTER || !process.env.STREAM_CHANNEL) {
-  console.log('Error: Specify CONSUMER_KEY CONSUMER_SECRET ACCESS_TOKEN_KEY ACCESS_TOKEN_SECRET STREAM_FILTER STREAM_CHANNEL in environment');
+if (!process.env.CONSUMER_KEY || !process.env.CONSUMER_SECRET || !process.env.ACCESS_TOKEN_KEY || !process.env.ACCESS_TOKEN_SECRET) {
+  console.log('Error: Specify CONSUMER_KEY CONSUMER_SECRET ACCESS_TOKEN_KEY ACCESS_TOKEN_SECRET in environment');
   process.exit(1);
 }
 
@@ -20,14 +20,14 @@ export default class TwitterStream {
     this.bot = bot;
   }
 
-  stream() {
-    twitterclient.stream('statuses/filter', {track: process.env.STREAM_FILTER}, (stream) => {
+  stream(channelId, filter) {
+    twitterclient.stream('statuses/filter', {track: filter}, (stream) => {
 
       stream.on('data', (tweet) => {
         const url = 'http://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str;
         this.bot.api.chat.postMessage({
           text: url,
-          channel: process.env.STREAM_CHANNEL,
+          channel: channelId,
           username: 'twitter',
           icon_emoji: ':bird:'
         });
